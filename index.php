a7f176df23061e5188536c0fbd7933a1f2fa6475
Alex Osborn <mralexosborn@gmail.com>
2024-09-22 16:01:19 -0400

    maybe finally fixing font?

--- index.php
+++ index.php
@@ -306,11 +306,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Strictly. Recipes.</title>
     <link rel="stylesheet" href="styles.css"> <!-- Link to the CSS file -->
-    <style>
-@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap');
-</style>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
+
 </head>
 <body>
 <div class="sidebar">
